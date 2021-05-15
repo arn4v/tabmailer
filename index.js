9af4e11d8e1f05495ffd83e54676ae50416e217e
Arnav Gosain <arnav@arnavgosain.com>
2021-05-15 11:27:09 +0000

    Clone onetab-backup on every cron run

--- index.js
+++ index.js
@@ -3,53 +3,66 @@ const nodemailer = require("nodemailer");
 const { dbPath } = require("./config");
 const sqlite3 = require("better-sqlite3");
 const cron = require("node-cron");
+const child_process = require("child_process")
 const htmlTemplate = String.raw;
 
 const main = async () => {
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.NODEMAILER_USER,
-      pass: process.env.NODEMAILER_PASS,
-    },
-  });
-  const conn = sqlite3(dbPath, {
-    readonly: true,
-  });
+	const transport = nodemailer.createTransport({
+		service: "Gmail",
+		auth: {
+			user: process.env.NODEMAILER_USER,
+			pass: process.env.NODEMAILER_PASS,
+		},
+	});
 
-  const sendEmail = () => {
-    Promise.all(
-      Array.from(Array(10).keys()).map(() =>
-        conn.prepare("SELECT * FROM links ORDER BY RANDOM() LIMIT 1").get()
-      )
-    ).then((rows) => {
-      const text = rows
-        .map((item, idx, arr) => {
-          return `${idx + 1}. ${item.title} - ${item.url}`;
-        })
-        .join("\n\n");
-      const html = htmlTemplate`
+	const sendEmail = async () => {
+		const clone_command = `git clone https://arn4v:${process.env.GITHUB_PAT}@github.com/arn4v/onetab-backup`
+		child_process.execSync("rm -rf onetab-backup")
+		child_process.execSync(clone_command, {
+			stdio: [0, 1, 2], // we need this so node will print the command output
+		})
+		child_process.execSync("cp ./onetab-backup/onetab.sqlite3 .")
+		const conn = sqlite3(dbPath, {
+			readonly: true,
+		});
+		const rows = await Promise.all(
+			Array.from(Array(10).keys()).map(() =>
+				conn.prepare("SELECT * FROM links ORDER BY RANDOM() LIMIT 1").get()
+			))
+		const text = rows
+			.map((item, idx, arr) => {
+				return `${idx + 1}. ${item.title} - ${item.url}`;
+			})
+			.join("\n\n");
+		const html = htmlTemplate`
         ${rows
-          .map((item, idx, arr) => {
-            return `<strong>${idx + 1}.</strong> <a href=${item.url}>${
-              item.title
-            }</a>
+				.map((item, idx, arr) => {
+					return `<strong>${idx + 1}.</strong> <a href=${item.url}>${
+						item.title
+						}</a>
             <br>`;
-          })
-          .join("\n\n")}
+				})
+				.join("\n\n")}
       `;
-      const date = new Date();
-      transport.sendMail({
-        subject: `Your links - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
-        to: process.env.RECEIVER_EMAIL,
-        text,
-        html,
-      });
-    });
-  };
+		const date = new Date();
+		try {
+			await transport.sendMail({
+				subject: `Your links - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`,
+				to: process.env.RECEIVER_EMAIL,
+				text,
+				html,
+			})
+			console.log("Sent email")
+		} catch (err) {
+			console.log(err)
+		}
 
-  sendEmail();
-  cron.schedule("0 08 */2 * *", sendEmail);
+		child_process.execSync("rm ./onetab.sqlite3")
+		child_process.execSync("rm -rf onetab-backup")
+	};
+
+	sendEmail();
+	cron.schedule("0 08 */2 * *", sendEmail);
 };
 
 main();
